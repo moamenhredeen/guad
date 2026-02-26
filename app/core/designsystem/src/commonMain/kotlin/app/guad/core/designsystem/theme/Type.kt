2db1e7edd522bd7c3a015e3e16a4f943b7725083
Moamen Hraden <moamen.hraden@primion.eu>
2026-02-26 21:12:54 +0100

    feat: define guad theme

--- app/core/designsystem/src/commonMain/kotlin/app/guad/core/designsystem/theme/Type.kt
+++ app/core/designsystem/src/commonMain/kotlin/app/guad/core/designsystem/theme/Type.kt
@@ -1,8 +1,114 @@
 package app.guad.core.designsystem.theme
 
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.text.font.FontFamily
-//
-//val PlusJakartaSans @Composable get() = FontFamily(
-////    Font()
-//)
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import guad.core.designsystem.generated.resources.PlusJakartaSans_Variable
+import guad.core.designsystem.generated.resources.Res
+import org.jetbrains.compose.resources.Font
+
+
+val PlusJakartaSans @Composable get() = FontFamily(
+    Font(
+        resource = Res.font.PlusJakartaSans_Variable,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resource = Res.font.PlusJakartaSans_Variable,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resource = Res.font.PlusJakartaSans_Variable,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resource = Res.font.PlusJakartaSans_Variable,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resource = Res.font.PlusJakartaSans_Variable,
+        weight = FontWeight.Bold
+    ),
+)
+
+val Typography.labelXSmall: TextStyle
+    @Composable get() = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 11.sp,
+        lineHeight = 14.sp
+    )
+
+val Typography.titleXSmall: TextStyle
+    @Composable get() = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    )
+
+val Typography @Composable get() = Typography(
+    titleLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        lineHeight = 36.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 26.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 18.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = PlusJakartaSans,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 11.sp,
+        lineHeight = 14.sp
+    ),
+)
