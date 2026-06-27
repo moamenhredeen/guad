408f1c659aea594de0beaa819f3fd6cbe9785816
Moamen Hraden <moamen@hredeen.com>
2026-06-27 14:39:50 +0200

    Move profile screens into feature module

--- app/lib/config/router/app_router.dart
+++ app/lib/config/router/app_router.dart
@@ -4,12 +4,12 @@ import 'package:go_router/go_router.dart';
 import 'package:guad/config/router/app_router_notifier.dart';
 import 'package:guad/config/router/screen_paths.dart';
 import 'package:guad/features/auth/presentation/screens/login_screen.dart';
-import 'package:guad/presentation/screens/account/account_screen.dart';
+import 'package:guad/features/profile/presentation/screens/account_screen.dart';
+import 'package:guad/features/profile/presentation/screens/personal_info/edit_personal_info_screen.dart';
+import 'package:guad/features/profile/presentation/screens/personal_info/personal_info_screen.dart';
+import 'package:guad/features/profile/presentation/screens/profile_screen.dart';
 import 'package:guad/presentation/screens/home/home_screen.dart';
 import 'package:guad/presentation/screens/notifications/notifications_screen.dart';
-import 'package:guad/presentation/screens/personal_info/edit_personal_info_screen.dart';
-import 'package:guad/presentation/screens/personal_info/personal_info_screen.dart';
-import 'package:guad/presentation/screens/profile/profile_screen.dart';
 import 'package:guad/presentation/screens/splash/splash_screen.dart';
 import 'package:guad/presentation/screens/tabs/tabs_screen.dart';
 
